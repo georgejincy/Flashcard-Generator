537179e55a38be42ab172d1d7b3d2f776b7eb500
georgejincy <jincydon101@gmail.com>
2017-02-20 12:36:49 -0500

    Added js files for flash cards and created constructors for BasicCard that are scope safe

---
+++ BasicCard.js
@@ -0,0 +1,11 @@
+//BasicCard constructor
+var BasicCard = function(front, back){
+	//Scope safe constructors
+	if(this instanceof BasicCard){
+		this.front = front;
+		this.back = back;
+	}else{
+		return new BasicCard(front,back);
+	}
+	
+};
