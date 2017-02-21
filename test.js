7cbc41c3edd5081ccb124ca47ed7566c54c0a5e0
georgejincy <jincydon101@gmail.com>
2017-02-21 11:21:48 -0500

    Create quiz objects with questions in test.js

--- test.js
+++ test.js
@@ -1,8 +1,32 @@
 var BasicCard = require("./BasicCard");
 var ClozeCard = require("./ClozeCard");
+var basicQuizObj = {
+	ques:[]
+};
+var clozeQuizObj = {
+	ques:[]
+};
+
+//Create quiz cards and push them to quiz array
+basicQuizObj.ques.push(new BasicCard("By area, what is the smallest country on the planet?", "Vatican City"));
+basicQuizObj.ques.push(new BasicCard("What is the name of the tallest uninterrupted waterfall in the world?", "Angel Falls"));
+basicQuizObj.ques.push(new BasicCard("What is the largest county in United States?", "San Bernardino"));
+basicQuizObj.ques.push(new BasicCard("After Alaska, which US state has the longest coastline?", "Florida"));
+
+console.log(JSON.stringify(basicQuizObj));
+
+clozeQuizObj.ques.push(new ClozeCard("By area, Vatican City is the smallest country on the planet", "Vatican City"));
+clozeQuizObj.ques.push(new ClozeCard("Angel Falls is the name of the tallest uninterrupted waterfall in the world", "Angel"));
+clozeQuizObj.ques.push(new ClozeCard("San Bernardino is the largest county in United States", "San Bernardino"));
+clozeQuizObj.ques.push(new ClozeCard("After Alaska, Florida is the next US state that has the longest coastline", "Florida"));
+
+console.log(JSON.stringify(clozeQuizObj));
+
+
+
 
 //Testing and debugging
-var firstPresidentBasic = new BasicCard(" Who was the first president of the United States?", "George Washington");
+/*var firstPresidentBasic = new BasicCard(" Who was the first president of the United States?", "George Washington");
 console.log(firstPresidentBasic.front);
 console.log(firstPresidentBasic.back);
 
@@ -10,4 +34,4 @@ var firstPresidentCloze = new ClozeCard(
     "George Washington was the first president of the United States.", "George Washington");
 console.log(firstPresidentCloze.partial);
 console.log(firstPresidentCloze.cloze);
-console.log(firstPresidentCloze.fullText);
+console.log(firstPresidentCloze.fullText);*/
