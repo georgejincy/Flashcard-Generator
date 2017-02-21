11244337c481ed3bd76feb9912d59ed3dd34b77b
georgejincy <jincydon101@gmail.com>
2017-02-21 13:19:29 -0500

    Add inquirer logic to ask questions from the json file

--- BasicCard.js
+++ BasicCard.js
@@ -1,5 +1,13 @@
 // dependency for inquirer npm package
 var inquirer = require("inquirer");
+// dependency for inquirer fs
+var fs = require('fs');
+var quizBasicObj;
+//variable to store score details
+var score = 0;
+//variable we will use to count how many times the questions have to be asked
+var count = 0;
+
 
 //BasicCard constructor
 var BasicCard = function(front, back){
@@ -14,3 +22,60 @@ var BasicCard = function(front, back){
 };
 
 module.exports = BasicCard;
+
+//Read bsic.json and get the quizObject
+fs.readFile('basic.json', 'utf8', function(err, data){
+	if(err) {
+        return console.log(err);
+    }
+    console.log(data);
+    //convert to object
+    quizBasicObj = JSON.parse(data);
+
+    console.log(quizBasicObj.ques.length);
+
+    // call askQuestion to playgame
+	askQuestion();
+});
+
+var askQuestion = function(){
+	//if statement that ensures we ask only the questions in the quizBasicObj
+	if(count < quizBasicObj.ques.length){
+		inquirer.prompt([
+  		{
+		    type: 'input',
+		    message: quizBasicObj.ques[count].front,
+		    name: 'userAns'
+		 }
+		]).then(function (answers) {
+		 console.log(answers.userAns);
+		 // add one to count to increment our recursive loop by one
+      	  count++;
+	      // run the askquestion function again so as to either end the loop or ask the questions again
+	      askQuestion();
+		});
+
+
+	}
+	//else statement which will output the final score for the game
+	else{
+		console.log("Game Over!\nYour score is: "+ score);
+		//Do you want to play again?
+		inquirer.prompt([{
+    		type: 'confirm',
+    		name: 'doPlayAgain',
+    		message: 'Do you want to play again?',
+    		default: false
+  		}]).then(function(answers) {
+  			console.log(answers.doPlayAgain);
+  			if(answers.doPlayAgain === true){
+	  			//reset count and score and call askQuestion
+	  			count = 0;
+	  			score = 0;
+	  			askQuestion();
+  			}
+		});
+	}
+
+}
+
